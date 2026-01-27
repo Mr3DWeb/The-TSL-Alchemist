b4ea573d582a8a0407ed780551d99624057ad0b1
Mohammad Sharyatzadeh <mr3dweb@gmail.com>
2026-01-27 14:54:39 +0330

    install leva

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import ReactLenis from 'lenis/react';
 import Experience from './components/Layout/Experience';
-//import Loader from './components/UI/Loader/Loader';
 
 function App() {
   return (
     <>
-      <ReactLenis root autoRaf={false} options={{lerp:0.05}}>
-        <Experience />
-      </ReactLenis>
+      <Experience />
     </>
   );
 }
